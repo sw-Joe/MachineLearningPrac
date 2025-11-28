4e720154fef32e117111acf1ebbb3ede91f630f2
Joe/remote <traceur0@kku.ac.kr>
2025-11-28 06:43:38 +0000

    model test function fixed

--- cat_n_dog/main.py
+++ cat_n_dog/main.py
@@ -4,6 +4,7 @@ import torch.cuda
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from pathlib import Path
 
 from common.model import CNN
 from common.preprocessing import CustomDataset
@@ -17,7 +18,7 @@ from cat_n_dog.config.parameter import BATCH_SIZE, IMG_FORMAT, PATH_ORIGINAL_CAT
 # if __name__ ==  '__main__':
 #     '''재현을 위한 시드 '''
 #     random.seed(SEED)
-MODEL_NAME = "test_"
+RUN_NAME = "classification_1"
 
 """  GPU 존재 확인 """
 DEVICE = torch.device("cpu")
@@ -34,8 +35,9 @@ dataset_dog = CustomDataset(PATH_ORIGINAL_DOG + IMG_FORMAT, label=1)
 
 
 """ customPackage.split을 이용한 데이터 분할 """
-cat_train, cat_val, cat_test = DatasetSplit(dataset_cat).t_v_t_split([0.8, 0.1, 0.1], MODEL_NAME)
-dog_train, dog_val, dog_test = DatasetSplit(dataset_dog).t_v_t_split([0.8, 0.1, 0.1], MODEL_NAME)
+PROPORTION = [0.7, 0.1, 0.2]
+cat_train, cat_val, cat_test = DatasetSplit(dataset_cat).t_v_t_split(PROPORTION, RUN_NAME)
+dog_train, dog_val, dog_test = DatasetSplit(dataset_dog).t_v_t_split(PROPORTION, RUN_NAME)
 
 train_set = cat_train + dog_train
 validation_set = cat_val + dog_val
@@ -49,27 +51,30 @@ valset_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=True)
 
 """ 모델, 옵티마이저, 비용함수 인스턴스 생성 """ 
 cnn_model = CNN().to(DEVICE)    # 모델 생성(+ 모델을 GPU로 이동)
-optimizer = optim.SGD(cnn_model.parameters(), lr=0.001, momentum=0.9)    # 옵티마이저 생성
+optimizer = optim.SGD(cnn_model.parameters(), lr=0.001, momentum=0.9)    # 옵티마이저 생성: Stochastic Gradient Descent
 criterion = nn.CrossEntropyLoss()    # 비용(손실)함수 객체 생성
 
 
 """ 학습 진행 """
-train(cnn_model, optimizer, criterion, trainset_loader, valset_loader, TOTAL_EPOCH)
+# train(cnn_model, optimizer, criterion, trainset_loader, valset_loader, TOTAL_EPOCH)
 
 
-
-""" 모델 저장 """
-path_saved = f"./{MODEL_NAME}_cat_n_dog_CNN.pth"
-print("model: ", path_saved)
+""" 모델 상태 저장 """
+path_save = f"./{RUN_NAME}_cat_n_dog_CNN.pth"
+print("model: ", path_save)
+torch.save(cnn_model.state_dict(), path_save)
 
 
 """ 모델 테스트 """
-cat_test = DatasetSplit(dataset_cat).load_trainset(MODEL_NAME)
-dog_test = DatasetSplit(dataset_dog).load_trainset(MODEL_NAME)
-testset_loader = cat_test + dog_test
+cat_test = DatasetSplit(dataset_cat).load_trainset(RUN_NAME)
+dog_test = DatasetSplit(dataset_dog).load_trainset(RUN_NAME)
+test_set = cat_test + dog_test
 
-model_test(cnn_model, path_saved, testset_loader)
+testset_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+
+
+path_model_status_saved = f"./cat_n_dog_CNN_batch_32.pth"
+
+model_test(cnn_model, path_model_status_saved, testset_loader)
 classes = ['cat', 'dog']
-model_test_each_class(cnn_model, path_saved, testset_loader, classes)
-
-torch.save(cnn_model.state_dict(), f"./{MODEL_NAME}_cat_n_dog_CNN.pth")
+model_test_each_class(cnn_model, path_model_status_saved, testset_loader, classes)
