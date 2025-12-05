0fec11efd98c6f4171e80e95eb1b9be83d389fa7
Joe/remote <traceur0@kku.ac.kr>
2025-12-05 09:27:32 +0000

    ongoing

--- ml_package/metric.py
+++ ml_package/metric.py
@@ -1,2 +1,32 @@
-def print_metric():
-    print("metric")
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, 
+                             f1_score, confusion_matrix, roc_auc_score)
+
+
+class metrics:
+    def __init__(self, test, predict):
+        self.test = test
+        self.predict = predict
+
+
+    def _accuracy(self):
+        return accuracy_score(self.test, self.predict)
+    
+
+    def _precision(self):
+        return precision_score(self.test, self.predict)
+    
+
+    def _recall(self):
+        return recall_score(self.test, self.predict)
+
+        
+    def _f1(self):
+        return f1_score(self.test, self.predict)
+
+
+    def _roc_auc(self):
+        return roc_auc_score(self.test, self.predict)
+    
+
+    def _confusion_matrix(self):
+        return confusion_matrix(self.test, self.predict)
