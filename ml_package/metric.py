1a88c01bd82d6b53dd2fd1dec461479959ab5631
Joe/remote <traceur0@kku.ac.kr>
2025-12-22 11:51:11 +0000

    sklearn metric func compilation

--- ml_package/metric.py
+++ ml_package/metric.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import (accuracy_score, precision_score, recall_score, 
                              f1_score, confusion_matrix, roc_auc_score)
 
 
-class metrics:
+class Metrics:
     def __init__(self, test, predict):
         self.test = test
         self.predict = predict
@@ -28,5 +30,11 @@ class metrics:
         return roc_auc_score(self.test, self.predict)
     
 
-    def _confusion_matrix(self):
-        return confusion_matrix(self.test, self.predict)
+    def _confusion_matrix(self) -> None:
+        sns.heatmap(confusion_matrix(self.test, self.predict), annot=True, fmt='d')
+        plt.xlabel('Prediction')
+        plt.ylabel('Real')
+        plt.savefig('confusion_matrix.png')
+        return
+    
+    # MAE, MSE, RMSE, MAPE
