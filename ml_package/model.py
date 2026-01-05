6b2db0ea6bea169133ed7a74d1e6c5d4ec4806e8
Joe/remote <traceur0@kku.ac.kr>
2026-01-05 13:39:42 +0000

    class Model2 added

--- ml_package/model.py
+++ ml_package/model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch import flatten
 
 """ 모델 정의 """
-class CNN(nn.Module):
+class Model1(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5)    # 입력 3채널, 출력 16채널
@@ -36,4 +36,30 @@ class CNN(nn.Module):
         x = self.dropout(x)
         
         x = self.fc3(x)
+        return x
+
+
+class Model2(nn.Module):
+    # 기존 모델을 개선 또는 이진분류시 성능이 더뛰어난 비교 목적의 모델을 작성
+    # 머신러닝 학습을 위한 간단한 모델
+
+    def __init__(self):
+        super().__init__()
+
+        # Feature extractor
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # 128 → 64 → 32
+        # 최종 feature map: 32 × 32 × 32
+        self.fc1 = nn.Linear(32 * 32 * 32, 64)
+        self.fc2 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 128 → 64
+        x = self.pool(F.relu(self.conv2(x)))  # 64 → 32
+        x = flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
