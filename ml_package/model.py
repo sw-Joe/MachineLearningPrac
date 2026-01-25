767d0f44847d96dff9f4fee891016ebd51583954
Joe/remote <traceur0@kku.ac.kr>
2026-01-25 16:12:14 +0000

    minor update

--- ml_package/model.py
+++ ml_package/model.py
@@ -116,7 +116,6 @@ class Model1ColorMulticlassMK3(nn.Module):    # RGB, 10 Classes
         return x
 
     
-
 class Model2(nn.Module):
     # 기존 모델을 개선 또는 이진분류시 성능이 더뛰어난 비교 목적의 모델을 작성
     # 머신러닝 학습을 위한 간단한 모델
@@ -140,46 +139,4 @@ class Model2(nn.Module):
         x = flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        return x
-    
-
-class AlexNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 96, 11, stride=4),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-
-            nn.Conv2d(96, 256, 5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-
-            nn.Conv2d(256, 384, 3, padding=1),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(384, 384, 3, padding=1),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(384, 256, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(True),
-
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-
-            nn.Linear(4096, 1000)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = flatten(x, 1)
-        x = self.classifier(x)
         return x
