6f9bc5dee7f3c98ad9b53efcd7e6322f634b3ca0
Joe/remote <traceur0@kku.ac.kr>
2026-01-25 16:07:00 +0000

    elapsed time logger seperated to logger.py

---
+++ ml_package/logger.py
@@ -0,0 +1,12 @@
+from time import perf_counter
+
+
+
+def count_time(func):
+    def wrapper(*args, **kwargs):
+        t_start = perf_counter()
+        f = func(*args, **kwargs)
+        t_end = perf_counter()
+        print("elapsed time: ", t_end-t_start)
+        return f
+    return wrapper
