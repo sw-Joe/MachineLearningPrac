600ced2ece991c85dd0e297c5c85cfb87b82a1bd
sw.J. <traceur0@kku.ac.kr>
2026-02-04 18:33:21 +0900

    main file for ResNet with ImageNet-100

--- ResNet/imagenet100.py
+++ ResNet/imagenet100.py
@@ -8,7 +8,7 @@ import wandb
 import torch.cuda
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import MultiStepLR
+from torch.optim.lr_scheduler import LambdaLR, MultiStepLR, SequentialLR
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from torchvision.transforms import InterpolationMode
@@ -47,7 +47,7 @@ def main(cfg: DictConfig):
     print(f"config : {CONFIG}")
 
     # True: 학습&평가 모드, False: 평가
-    FLAG = True
+    FLAG = False
 
     """ Wandb 기록 여부 플래그에 따른 초기화 """
     if cfg.wandb.enabled:
@@ -128,8 +128,8 @@ def main(cfg: DictConfig):
 
 
     """ 모델 인스턴스 생성 """
-    model = ResNetImageNet(Bottleneck, [3, 4, 6, 3], num_classes=100)    ### 모델 객체 생성(+ 모델을 GPU로 이동)
-    # model = ResNetImageNet(Bottleneck, [3, 4, 23, 3], num_classes=100)
+    # model = ResNetImageNet(Bottleneck, [3, 4, 6, 3], num_classes=100)    ### 모델 객체 생성(+ 모델을 GPU로 이동)
+    model = ResNetImageNet(Bottleneck, [3, 4, 23, 3], num_classes=100)
         ### 모델 객체 생성(+ 모델을 GPU로 이동)
 
     if torch.cuda.device_count() > 1:
@@ -143,15 +143,15 @@ def main(cfg: DictConfig):
         optimizer = optim.SGD(model.parameters(), lr=cfg.optimizer.lr, 
                               momentum=cfg.optimizer.momentum, weight_decay=cfg.optimizer.w_decay)    # 옵티마이저 생성: Stochastic Gradient Descent
 
-        scheduler = MultiStepLR(optimizer, milestones=cfg.scheduler.milestones, gamma=cfg.scheduler.gamma) # 에폭 기준 예시
+        # scheduler = MultiStepLR(optimizer, milestones=cfg.scheduler.milestones, gamma=cfg.scheduler.gamma) # 에폭 기준 예시
 
         # # 1. 웜업 스케줄러 (400 step까지 0.1배 적용)
-        # warmup_sch = LambdaLR(optimizer, lr_lambda=lambda step: 0.1 if step < 400 else 1.0)
+        warmup_sch = LambdaLR(optimizer, lr_lambda=lambda step: 0.1 if step < 400 else 1.0)
         # # 2. 메인 스케줄러 (30, 60, 90 에폭에서 감쇠)
-        # main_sch = MultiStepLR(optimizer, milestones=cfg.scheduler.milestones, gamma=cfg.scheduler.gamma)
+        main_sch = MultiStepLR(optimizer, milestones=cfg.scheduler.milestones, gamma=cfg.scheduler.gamma)
 
         # # 3. 통합 (400 step 지점에서 메인으로 전환)
-        # scheduler = SequentialLR(optimizer, schedulers=[warmup_sch, main_sch], milestones=[400])
+        scheduler = SequentialLR(optimizer, schedulers=[warmup_sch, main_sch], milestones=[400])
 
         criterion = nn.CrossEntropyLoss()    # 비용(손실)함수 객체 생성
 
@@ -177,7 +177,7 @@ def main(cfg: DictConfig):
     if FLAG:
         pass
     else:
-        NOW = "26-00-00_00-00-00"    # 테스트에 사용할 모델의 run_name(시간정보)를 명시
+        NOW = "26-02-03_10-10-54"    # 테스트에 사용할 모델의 run_name(시간정보)를 명시
 
     best_model = f"./{cfg.artifact.dir}{NOW}/best_model_{NOW}.pt"
     
